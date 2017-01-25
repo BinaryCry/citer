f8c71e449344c8c1e889bd0acc46185630207c49
Alex Step <stepalexandr@gmail.com>
2017-01-25 15:52:27 +0200

    text/html

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// @server 1.0
+// @server 1.1
 
 const http = require('http');
 const PORT = 3000;
@@ -6,6 +6,9 @@ const IP   = '127.0.0.2';
 
 respreq = (req,res) => {
     console.log('1');
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('<br>');
+    res.end('<h1>Hello, World!</h1>');
 };
 
 const server = http.createServer(respreq);
