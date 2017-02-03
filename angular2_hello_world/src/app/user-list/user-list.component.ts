d2de925e9eecd0a0c2bbd2f425efaf317e3464ce
Alex Step <stepalexandr@gmail.com>
2017-02-03 17:18:09 +0200

    app:hw+users

---
+++ angular2_hello_world/src/app/user-list/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+    names: Array<string>; // string[]
+
+    constructor() {
+        this.names = ['Carl','Albert','Monica'];
+    }
+
+    ngOnInit() {
+
+    }
+
+}
