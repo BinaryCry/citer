06a5e3302296b7cebac35c021d0d67450ab1cca3
Alex Step <stepalexandr@gmail.com>
2017-06-02 17:09:49 +0300

    React lernen 2

--- graphQL/web/js/index.js
+++ graphQL/web/js/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 // const un = <b>%username%!</b>;
 
 var hw = React.createElement(
@@ -27,6 +35,7 @@ var element = React.createElement('div', { className: 'greeting', id: 'wrapper' 
 
 ReactDOM.render(element, document.getElementById('root'));
 
+//--------------------------------------------------------------
 var itrvArr = [];
 itrvArr.push(setInterval(function () {
     ReactDOM.render(React.createElement(
@@ -35,7 +44,6 @@ itrvArr.push(setInterval(function () {
         new Date().toLocaleString()
     ), document.getElementById('time'));
 }, 1000));
-
 document.getElementById('stopItrvs').onclick = function (e) {
     if (itrvArr.length < 1) return false;else {
         for (var i = 0; i < itrvArr.length; i++) {
@@ -43,4 +51,138 @@ document.getElementById('stopItrvs').onclick = function (e) {
         }
     }
 };
+//--------------------------------------------------------------
+function FooRoot(props) {
+    return React.createElement(
+        'p',
+        null,
+        'Function Component ',
+        props.data
+    );
+}
+var fooRootElem = React.createElement(FooRoot, { data: 'PropsData' });
+ReactDOM.render(fooRootElem, document.getElementById('fooRoot'));
+//--------------------------------------------------------------
+
+var ClassRoot = function (_React$Component) {
+    _inherits(ClassRoot, _React$Component);
+
+    function ClassRoot() {
+        _classCallCheck(this, ClassRoot);
+
+        return _possibleConstructorReturn(this, (ClassRoot.__proto__ || Object.getPrototypeOf(ClassRoot)).apply(this, arguments));
+    }
+
+    _createClass(ClassRoot, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'p',
+                null,
+                'Class Component ',
+                this.props.data
+            );
+        }
+    }]);
+
+    return ClassRoot;
+}(React.Component);
+
+var classRootElem = React.createElement(ClassRoot, { data: 'ClassRoot' });
+ReactDOM.render(classRootElem, document.getElementById('classRoot'));
+//--------------------------------------------------------------
+function foo() {
+    return 111;
+}
+function HelloUser(props) {
+    return React.createElement(
+        'h5',
+        null,
+        'Hello ',
+        props.name
+    );
+}
+function AllHello() {
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(HelloUser, { name: 'Alex' }),
+        React.createElement(HelloUser, { name: 'Tina' }),
+        React.createElement(HelloUser, { name: 'Arnold' }),
+        foo()
+    );
+}
+ReactDOM.render(React.createElement(AllHello, null), document.getElementById('allHello'));
+//--------------------------------------------------------------
+
+var elem = document.getElementById('mainComponent');
+
+var MainC = function (_React$Component2) {
+    _inherits(MainC, _React$Component2);
+
+    function MainC(props) {
+        _classCallCheck(this, MainC);
+
+        var _this2 = _possibleConstructorReturn(this, (MainC.__proto__ || Object.getPrototypeOf(MainC)).call(this, props));
+
+        _this2.state = { dateTime: new Date() };
+        return _this2;
+    }
+
+    _createClass(MainC, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement('hr', null),
+                React.createElement(
+                    'h3',
+                    null,
+                    'MainC'
+                ),
+                React.createElement(
+                    'p',
+                    null,
+                    this.state.dateTime.toLocaleString()
+                ),
+                React.createElement(
+                    'p',
+                    null,
+                    this.props.value
+                )
+            );
+        }
+    }, {
+        key: 'tick',
+        value: function tick() {
+            this.setState({
+                dateTime: new Date()
+            });
+        }
+    }, {
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            var _this3 = this;
+
+            this.timerID = setInterval(function () {
+                _this3.tick();
+            }, 1000);
+
+            // setTimeout( ()=> {
+            //    this.props.value = 1000;
+            //    this.setState(); // can be used for rewrite View with chanded data
+            // }, 1500  )
+        }
+    }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+            clearInterval(this.timerID);
+        }
+    }]);
+
+    return MainC;
+}(React.Component);
+
+ReactDOM.render(React.createElement(MainC, { value: 1 }), elem);
 
--- graphQL/web/js/index.jsx
+++ graphQL/web/js/index.jsx
@@ -28,6 +28,8 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+
+//--------------------------------------------------------------
 let itrvArr = [];
 itrvArr.push(
     setInterval( ()=>{
@@ -37,7 +39,6 @@ itrvArr.push(
         )
     } , 1000)
 );
-
 document.getElementById('stopItrvs').onclick = (e) => {
     if( itrvArr.length < 1 ) return false;
     else {
@@ -46,3 +47,81 @@ document.getElementById('stopItrvs').onclick = (e) => {
         }
     }
 };
+//--------------------------------------------------------------
+function FooRoot(props) {
+    return <p>Function Component {props.data}</p>
+}
+const fooRootElem = <FooRoot data="PropsData" />;
+ReactDOM.render(fooRootElem, document.getElementById('fooRoot'));
+//--------------------------------------------------------------
+class ClassRoot extends React.Component {
+    render() {
+        return <p>Class Component {this.props.data}</p>
+    }
+}
+const classRootElem = <ClassRoot data="ClassRoot" />;
+ReactDOM.render(classRootElem, document.getElementById('classRoot'));
+//--------------------------------------------------------------
+function foo() {
+    return 111;
+}
+function HelloUser(props) {
+    return <h5>Hello {props.name}</h5>
+}
+function AllHello() {
+    return (
+        <div>
+            <HelloUser name="Alex" />
+            <HelloUser name="Tina" />
+            <HelloUser name="Arnold" />
+            { foo() }
+        </div>
+    )
+}
+ReactDOM.render(
+    <AllHello />,
+    document.getElementById('allHello')
+);
+//--------------------------------------------------------------
+
+const elem = document.getElementById('mainComponent');
+
+class MainC extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { dateTime: new Date() }
+    }
+    render() {
+        return (
+            <div>
+                <hr/>
+                <h3>MainC</h3>
+                <p>{ this.state.dateTime.toLocaleString() }</p>
+                <p>{ this.props.value }</p>
+            </div>
+        )
+    }
+    tick() {
+        this.setState(
+            {
+                dateTime: new Date()
+            }
+        );
+    }
+    componentDidMount() {
+        this.timerID = setInterval( () => {
+            this.tick();
+        }, 1000);
+
+        // setTimeout( ()=> {
+        //    this.props.value = 1000;
+        //    this.setState(); // can be used for rewrite View with chanded data
+        // }, 1500  )
+
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+}
+
+ReactDOM.render( <MainC value={ 1 } />, elem );
