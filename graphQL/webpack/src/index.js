c283450a30c507dbfc99e655bedd7d3e0adf6ef3
Alex Step <stepalexandr@gmail.com>
2017-07-24 12:11:06 +0300

    webpack 1

--- graphQL/webpack/src/index.js
+++ graphQL/webpack/src/index.js
@@ -1,1 +1,15 @@
-document.getElementById('root').innerHTML = "111";
+import './style.css'
+import doc from './img/doc.jpg'
+import jsonData from './data/data.json'
+
+const root = document.getElementById('root');
+
+root.innerHTML = "Hello WebPack";
+
+let img = new Image();
+img.src = doc;
+
+root.appendChild(img);
+
+console.log(jsonData);
+
--- graphQL/webpack/webpack.config.js
+++ graphQL/webpack/webpack.config.js
@@ -5,5 +5,28 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg|png|bmp|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|otf|ttf|eot)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
     }
 };
